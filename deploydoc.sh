981bd366ac89dcb1d7235306cf6c8c69aa8c5d35
Stefano Zaghi <stefano.zaghi@gmail.com>
2015-06-10 15:46:01 +0200

    Correct travis ci makedoc script

--- deploydoc.sh
+++
@@ -1,4 +0,0 @@
-cd doc/html
-git add -f --all *
-git commit -m "Travis CI autocommit from travis build ${TRAVIS_BUILD_NUMBER}"
-git push -fq origin gh-pages
--- makedoc.sh
+++ makedoc.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 git config --global user.name "Stefano Zaghi"
 git config --global user.email "stefano.zaghi@gmail.com"
 git clone --branch=gh-pages https://${GH_TOKEN}@github.com/szaghi/FLAP doc/html
+FoBiS.py rule -ex makedoc
+cd doc/html
+git add -f --all *
+git commit -m "Travis CI autocommit from travis build ${TRAVIS_BUILD_NUMBER}"
+git push -fq origin gh-pages
